a83b1d012550191cb34648274e1992a44f80e0cc
Tony <syntony666@outlook.com>
2019-09-28 00:10:04 +0800

    first commit

---
+++ src/main/java/Vector.java
@@ -0,0 +1,72 @@
+public class Vector {
+  private double[] _v;
+  public Vector(double ... elements) {
+    _v=elements;
+  }
+  public double getElement(int index) {
+    return _v[index];
+  }
+  public int getDim(){
+    return _v.length;
+  }
+  public boolean isZero() {
+    for(int i=0;i<_v.length;i++){
+      if(_v[i]!=0)
+        return false;
+    }
+    return true;
+  }
+  public double length() {
+    double res = 0;
+    for (double i : _v) {
+      res += Math.pow(i, 2);
+    }
+    return Math.sqrt(res);
+  }
+}
+
+class Calculate {
+  public static double dot(Vector u,Vector v) {
+    var result = 0;
+    if(v.getDim()!=u.getDim())
+      throw new ArrayIndexOutOfBoundsException();
+    for(int i = 0;i<u.getDim();i++)
+      result += u.getElement(i)*v.getElement(i);
+    return result;
+  }
+  public static Vector add(Vector u,Vector v) {    
+    if(u.getDim()!=v.getDim())
+      throw new ArrayIndexOutOfBoundsException();
+    double[] res = new double[u.getDim()];
+    for(int i = 0;i<u.getDim();i++)
+      res[i]=u.getElement(i)+v.getElement(i);
+    return new Vector(res);
+  }
+  public static Vector substract(Vector u,Vector v) {
+    if(u.getDim()!=v.getDim())
+      throw new ArrayIndexOutOfBoundsException();
+    double[] res = new double[u.getDim()];
+    for(int i = 0;i<u.getDim();i++)
+      res[i]=u.getElement(i)-v.getElement(i);
+    return new Vector(res);
+  }
+  public static double distance(Vector u,Vector v) {
+    return substract(u, v).length();
+  }
+  public static double area(Vector u, Vector v, Vector w) {
+    var s = (Calculate.distance(u, v)+Calculate.distance(v, w)+Calculate.distance(u, w))/2;
+    return Math.sqrt(s*(s-Calculate.distance(u, v))*(s-Calculate.distance(v, w))*(s-Calculate.distance(u, w)));
+  }
+  public static double area(Vector ... v) {
+    double result = 0;
+    for(int i=1;i<v.length-1;i++)
+      result += area(v[0], v[i], v[i+1]);
+    return result;
+  }
+  public static double perimeter(Vector ... v) {
+    double result = distance(v[0], v[v.length-1]);
+    for(int i=0;i<v.length-1;i++)
+      result += distance(v[i], v[i+1]);
+    return result;
+  }
+}
---
+++ src/test/java/VectorTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.*;
+
+public class VectorTest {
+  @Test
+  public void evaluatesExpression() {
+    Vector v = new Vector(2,3,5);
+    Assertions.assertEquals(5,v.getElement(2));
+  }
+  @Test
+  public void dotTest() {
+    Vector u = new Vector(2,3,5);
+    Vector v = new Vector(8,5,6);
+    Assertions.assertEquals(61,Calculate.dot(u,v));
+  }
+  @Test
+  public void dotExceptionTest() {
+    Vector u = new Vector(2,3,5);
+    Vector v = new Vector(8,5);
+    Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+      Calculate.dot(u,v);
+    });
+  }
+  @Test
+  public void isZeroTest() {
+    Vector u = new Vector(0,3,5);
+    Vector v = new Vector(0,0,0);
+    Assertions.assertEquals(false,u.isZero());
+    Assertions.assertEquals(true,v.isZero());
+  }
+  @Test
+  public void lengthTest() {
+    Vector u = new Vector(0,3,4);
+    Assertions.assertEquals(5,u.length(),0.001);
+  }
+  @Test
+  public void addTest() {
+    Vector u = new Vector(2,3,5);
+    Vector v = new Vector(8,5,6);
+    Vector w = Calculate.add(u,v);
+    Assertions.assertAll("Vector w",
+      ()-> Assertions.assertEquals(10,w.getElement(0)),
+      ()-> Assertions.assertEquals(8,w.getElement(1)),
+      ()-> Assertions.assertEquals(11,w.getElement(2))
+    );
+  }
+  @Test
+  public void substractTest() {
+    Vector u = new Vector(2,3,5);
+    Vector v = new Vector(8,5,6);
+    Vector w = Calculate.substract(u,v);
+    Assertions.assertAll("Vector w",
+      ()-> Assertions.assertEquals(-6,w.getElement(0)),
+      ()-> Assertions.assertEquals(-2,w.getElement(1)),
+      ()-> Assertions.assertEquals(-1,w.getElement(2))
+    );
+  }
+  @Test
+  public void distanceTest() {
+    Vector u = new Vector(2,3,5);
+    Vector v = new Vector(8,5,6);
+    Assertions.assertEquals(Math.sqrt(41),Calculate.distance(u,v));
+  }
+  @Test
+  public void area1Test() {
+    Vector u = new Vector(0,5,0);
+    Vector v = new Vector(0,10,0);
+    Vector w = new Vector(0,9,6);
+    Assertions.assertEquals(15,Calculate.area(u,v,w),0.001);
+  }
+  @Test
+  public void area2Test() {
+    Assertions.assertEquals(18,
+      Calculate.area(
+        new Vector(5,0),new Vector(10,0),new Vector(9,6),new Vector(8,6)),0.001);
+  }
+  @Test
+  public void perimeterTest() {
+    Assertions.assertEquals(6+2*Math.sqrt(2),
+      Calculate.perimeter(
+        new Vector(0,0),new Vector(2,0),new Vector(2,2),new Vector(1,3),new Vector(0,2)),0.001);
+  }
+}
